979c8f4e9c16ec6b68719bd85003a3f683d40ada
Milene <badouxmilene@gmail.com>
2017-12-11 13:30:27 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quote Machine#
+Free-code camp project for the Front End Certification
+The user can click a button to show a new random quote.
+He can press a button to tweet out a quote.
+
+## How to start ##
+Download the repository in your desktop, unzip it and open the "index.html" file in your browser to get started.
---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(data) {
+    $("#the-quote").append(data[0].content);
+    $("#the-source").append(data[0].title);
+  });
+  //Click on the new quote button to generate a new quote.
+  $("#new-quote").click(function() {
+    $("#the-quote").html('');
+    $("#the-source").html('');
+    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {
+      $("#the-quote").append(data[0].content);
+      $("#the-source").append(data[0].title);
+    });
+    //update the background color
+    var newColor = Math.floor(Math.random() * 16777215).toString(16);
+    newColor = "#" + ("000000" + newColor).slice(-6);
+    $("#new-quote").css("background", newColor);
+    $("body").css("background", newColor);
+    $(".zocial-twitter").css("background", newColor);
+  });
+  //Click on the twitter icon open the twitter page
+  var quote = document.getElementById('the-quote');
+  var source = document.getElementById('the-source');
+  var twitterIcon = document.getElementsByClassName('fa-twitter-square');
+  $(".fa-twitter-square").click(function() {
+    var newurl = "https://twitter.com/intent/tweet?text=" + quote.innerText + source.innerText + "&hashtags=quote";
+    window.open(newurl);
+  });
+
+});
