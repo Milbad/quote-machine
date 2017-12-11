0c19936bea00395ec02f83373d964790e7455fa7
Milene <badouxmilene@gmail.com>
2017-12-11 14:36:10 -0800

    style: update the background-color

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     var newBrightness = Math.random();
     var newColor = Math.floor(Math.random() * 16777215).toString(16);
     //newColor = "#" + ("000000" + newColor).slice(-6);
-    $("#quotebox").css('filter:brightness', '(' + newBrightness + ')');
+    $("#quotebox").css('filter', 'brightness(' + newBrightness + ')');
     //$("#quotebox").css("filter:brightness", newBrightness);
     //("body").css("background-color", newColor);
     //$(".fa-twitter-square").css("background-color", newColor);
